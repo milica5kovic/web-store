38309727ccc055e3a57795b2c2a910484dc3e7fe
milica5kovic <mpetkovic452@gmail.com>
2025-03-30 23:10:41 +0200

    Add readme

--- mpetkovic9322it.sql
+++ mpetkovic9322it.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 30, 2025 at 09:49 PM
+-- Generation Time: Mar 30, 2025 at 11:09 PM
 -- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- PHP Version: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -84,7 +84,23 @@ INSERT INTO `order` (`id`, `product_id`, `date`) VALUES
 (24, 2, '2025-03-01 20:14:38'),
 (25, 2, '2025-03-30 19:15:48'),
 (26, 2, '2025-03-30 19:15:48'),
-(27, 2, '2025-03-30 19:15:48');
+(27, 2, '2025-03-30 19:15:48'),
+(28, 2, '2025-03-30 20:30:19'),
+(29, 2, '2025-03-30 20:30:21'),
+(30, 2, '2025-03-30 20:30:26'),
+(31, 2, '2025-03-30 20:30:28'),
+(32, 2, '2025-03-30 20:30:29'),
+(33, 2, '2025-03-30 20:30:30'),
+(34, 2, '2025-03-30 20:30:32'),
+(35, 2, '2025-03-30 20:30:34'),
+(36, 2, '2025-03-30 20:30:34'),
+(37, 2, '2025-03-30 20:30:35'),
+(38, 2, '2025-03-30 20:30:36'),
+(39, 2, '2025-03-30 20:30:36'),
+(40, 2, '2025-03-30 20:30:37'),
+(41, 2, '2025-03-30 20:30:37'),
+(42, 2, '2025-03-30 20:30:38'),
+(43, 20, '2025-03-30 21:03:47');
 
 -- --------------------------------------------------------
 
@@ -109,8 +125,15 @@ CREATE TABLE `product` (
 --
 
 INSERT INTO `product` (`id`, `category`, `created_by`, `name`, `description`, `image_url`, `stock`, `price`, `mark`) VALUES
-(1, 1, 1, 'IPhone 16e', '<p>iPhone 16e has an incredible design &mdash; inside and out &mdash; and is available in an elegant black or white finish. Made from durable aerospace‑grade aluminum, the enclosure on iPhone 16e is built to go the distance and survive life&rsquo;s oops, whoops, and oh nooos.</p>', 'https://www.telekoplus.com/repo/vesti/images/iphone-16e-all-colors.jpg', 11, 699.9, 1),
-(2, 1, 1, 'IPhone 16', '<p>iPhone 16 has an incredible design &mdash; inside and out &mdash; and is available in an elegant black or white finish. Made from durable aerospace‑grade aluminum, the enclosure on iPhone 16 is built to go the distance and survive life&rsquo;s oops, whoops, and oh nooos.</p>', 'https://www.telekoplus.com/repo/vesti/images/iphone-16e-all-colors.jpg', 15, 899.9, 1);
+(1, 1, 1, 'IPhone 16e', '<p>iPhone 16e has an incredible design &mdash; inside and out &mdash; and is available in an elegant black or white finish. Made from durable aerospace‑grade aluminum, the enclosure on iPhone 16e is built to go the distance and survive life&rsquo;s oops, whoops, and oh nooos.</p>', '67e9ab7e036ae-16e.png', 11, 699.9, 1),
+(2, 1, 1, 'IPhone 16', '<p>iPhone 16 has an incredible design &mdash; inside and out &mdash; and is available in an elegant black or white finish. Made from durable aerospace‑grade aluminum, the enclosure on iPhone 16 is built to go the distance and survive life&rsquo;s oops, whoops, and oh nooos.</p>', '67e9ad4f4dc63-16e.png', 0, 899.9, 0),
+(15, 9, 1, 'Arduino Uno', 'Arduino is an open-source electronics platform based on easy-to-use hardware and software. Arduino boards are able to read inputs - light on a sensor, a finger on a button, or a Twitter message - and turn it into an output - activating a motor, turning on an LED, publishing something online.', '67e9ae6eea9ca-Arduino-uno-perspective-transparent.png', 5, 25, 0),
+(16, 10, 1, 'Raspberry Pi ', '<p>Raspberry Pi is the name of a series of single-board computers made by the Raspberry Pi Foundation, a UK charity that aims to educate people in computing and create easier access to computing education. The Raspberry Pi launched in 2012, and there have been several iterations and variations released since then.</p>', '67e9af60c8a11-RASP_03_01-1.png', 10, 28, 1),
+(17, 8, 1, 'Cisco1841 Router', '<p>Founded on 20 years of leadership and innovation, the modular Cisco&reg; 1800 Series of integrated services routers intelligently embed data and security into a single, resilient system for fast, scalable delivery of mission-critical business applications. The best-in class Cisco 1800 Series architecture has been specifically designed to meet requirements of small-to-medium- sized businesses, small enterprise branch offices, and service provider-managed services applications for delivery of concurrent services at wire-speed performance. The integrated secure systems architecture of the Cisco 1800 Series delivers maximum business agility and investment protection.</p>', '67e9af9f20da7-CS-CISCO1841-UPP_b.png', 3, 300, 1),
+(18, 7, 1, 'Xiaomi Light', 'Product modelMJYD02YL-A  \r\nOperating temperature-10℃ to 40℃\r\nProduct dimensionsΦ 80 x 62 mm\r\nRated voltage4.5 V⎓\r\nRated power0.35 W\r\nLuminous flux2.5 — 25 lm\r\nSupported systems: Android 4.4 or iOS 9.0 and above', '67e9b00a8cc00-1beecda60b18868b82e8d9acac03d0c3.png', 20, 15, 0),
+(19, 6, 1, 'You\'re being watched!', '<p>Hi Vule!</p>', '67e9b03ee7362-pms_1635511893.82491848.png', 1, 666, 1),
+(20, 4, 1, 'Xiaomi Camera 2', '    Functions: Record | Photo Capture | Alert\r\n    Ideal For: Private Space\r\n    Night Vision\r\n    AI Powered Motion Detection\r\n    Works with Google Assistant, Amazon Alexa\r\n    12 Months Warranty', '67e9b120acc04-cc.png', 2, 265.9, 0),
+(21, 5, 1, 'Smart Home Hub 2', '<p>Dual-band Wi-Fi connection<br>Wi-Fi, Zigbee, and Bluetooth connectivity<br>Dual-core 1GHz processor</p>', '67e9b161162bd-xiaomi-smart-home-hub-2-5676.png', 1, 1000, 1);
 
 -- --------------------------------------------------------
 
@@ -187,13 +210,13 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `order`
 --
 ALTER TABLE `order`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
 
 --
 -- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT for table `product_category`
---
+++ readme.md
@@ -0,0 +1,7 @@
+A simple, modern web store demo showcasing e-commerce functionality with a clean UI and smooth user experience. This project includes:
+
+✅ Product Listings – Display items dynamically with images, descriptions, and prices.
+✅ Admin Panel - Add, update, delete or remove categories.
+✅ Checkout Process – Simulated checkout flow for a seamless purchase experience.
+✅ Responsive Design – Works on desktops, tablets, and mobile devices.
+✅ Tech Stack – Built with PHP 8, Bootstrap, JS.
